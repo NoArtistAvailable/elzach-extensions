3fb8d2776a6ac6be4bfc2cef115d5f941317835e
zach <felix.reinemann@gmail.com>
2020-04-14 12:59:43 +0200

    [Extension] added extensions from workproject

---
+++ Runtime/MiscExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace elZach.Common
+{
+    public static class MiscExtension
+    {
+        public static Keyframe GetLastKey(this AnimationCurve curve)
+        {
+            return curve.keys[curve.keys.Length - 1];
+        }
+    }
+}
--- Runtime/Vector3Extension.cs
+++ Runtime/Vector3Extension.cs
@@ -1,11 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class Vector3Extension
+namespace elZach.Common
 {
-    public static Vector2 xy(this Vector3 vector)
+    public static class Vector3Extension
     {
-        return new Vector2(vector.x, vector.y);
+
+        public static Vector3 ZeroY(this Vector3 xyz)
+        {
+            return new Vector3(xyz.x, 0f, xyz.z);
+        }
+
+        public static Vector3 XZDirection(this Vector3 xyz)
+        {
+            return new Vector3(xyz.x, 0f, xyz.z).normalized;
+        }
+
+        public static Vector3 YZDirection(this Vector3 xyz)
+        {
+            return new Vector3(0f, xyz.y, xyz.z).normalized;
+        }
+
+        public static Vector2 xz(this Vector3 xyz)
+        {
+            return new Vector2(xyz.x, xyz.z);
+        }
+
+        public static Vector2 xy(this Vector3 xyz)
+        {
+            return new Vector2(xyz.x, xyz.y);
+        }
     }
 }
