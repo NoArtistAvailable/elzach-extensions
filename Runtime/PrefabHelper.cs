e145c3572b943ee362d14d9ab448a8be4c054bd7
zach <felix.reinemann@gmail.com>
2020-04-17 09:59:17 +0200

    [PrefabHelper] added OnPrefabSaving event

---
+++ Runtime/PrefabHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine.Events;
+
+namespace elZach.Common
+{
+    public class PrefabHelper : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public UnityEvent OnSavingPrefab;
+        public void SavingPrefab(GameObject go)
+        {
+            OnSavingPrefab.Invoke();
+        }
+
+        private void OnEnable()
+        {
+            PrefabStage.prefabSaving += SavingPrefab;
+        }
+
+        private void OnDisable()
+        {
+            PrefabStage.prefabSaving -= SavingPrefab;
+        }
+#endif
+    }
+}
