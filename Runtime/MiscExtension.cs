cda4e52538634ea2b2a6152098c0b4cd458b39a7
zach <felix.reinemann@gmail.com>
2020-05-03 16:22:59 +0200

    [Texture2D] extension to get copy from non-readable textures

--- Runtime/MiscExtension.cs
+++ Runtime/MiscExtension.cs
@@ -10,5 +10,25 @@ namespace elZach.Common
         {
             return curve.keys[curve.keys.Length - 1];
         }
+
+        public static Texture2D GetCopy(this Texture2D source)
+        {
+            RenderTexture tmp = RenderTexture.GetTemporary(
+                source.width,
+                source.height,
+                0,
+                RenderTextureFormat.Default,
+                RenderTextureReadWrite.Linear);
+
+            Graphics.Blit(source, tmp);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = tmp;
+            Texture2D copy = new Texture2D(source.width, source.height);
+            copy.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
+            copy.Apply();
+            RenderTexture.active = previous;
+
+            return copy;
+        }
     }
 }
