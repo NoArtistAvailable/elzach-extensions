68e653e8aa8b70591fbf602b95d922d285cf2f18
zach <felix.reinemann@gmail.com>
2020-09-29 18:06:40 +0200

    added AnimationEventRelay

---
+++ Runtime/AnimationEventRelay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace elZach.Common
+{
+    public class AnimationEventRelay : MonoBehaviour
+    {
+        public UnityEvent[] events;
+        public void CallEvent(int index)
+        {
+            events[index].Invoke();
+        }
+    }
+}
