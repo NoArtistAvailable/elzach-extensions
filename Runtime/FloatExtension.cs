785928cc8f2f2c42da56620fff79c2f4cdac97e2
zach <felix.reinemann@gmail.com>
2021-02-01 11:58:30 +0100

    [Float] added Remap overload for 0f-1f range

--- Runtime/FloatExtension.cs
+++ Runtime/FloatExtension.cs
@@ -6,5 +6,10 @@
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
+        public static float Remap(this float value, float from, float to)
+        {
+            //return (value - 0f) / (1f - 0f) * (to - from) + from;
+            return value * (to - from) + from;
+        }
     }
 }
