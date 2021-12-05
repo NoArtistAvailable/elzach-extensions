f06aa373700436a49f793725c59d61fb82788347
zach <el-zach@web.de>
2021-12-05 14:52:24 +0100

    [Delay] scaled time

--- Runtime/WebGL/WebTask.cs
+++ Runtime/WebGL/WebTask.cs
@@ -7,6 +7,12 @@ namespace elZach.Common
     {
         public static async Task Delay(float value)
         {
+            float endTime = Time.time + value;
+            while (Time.time < endTime) await Task.Yield();
+        }
+		
+		public static async Task DelayUnscaled(float value)
+        {
             float endTime = Time.unscaledTime + value;
             while (Time.unscaledTime < endTime) await Task.Yield();
         }
