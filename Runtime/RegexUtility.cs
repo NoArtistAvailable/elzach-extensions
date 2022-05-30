6bc62c91cda32d53723b5a3d6387b08f7e08db93
zach <felix.reinemann@gmail.com>
2022-05-30 13:22:47 +0200

    [Utility] added regex utility to get indices & names for list properties

---
+++ Runtime/RegexUtility.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace elZach.Common
+{
+    public static class RegexUtility
+    {
+        private static readonly Regex indexRegex = new Regex(@"[(?<index>\d+)]$", RegexOptions.Compiled);
+        private static readonly Regex indexAndListRegex = new Regex(@"(?<listname>\w+)\.Array\.data\[(?<index>\d+)\]$", RegexOptions.Compiled);
+
+        public static bool TryGetLastIndex(string path, out int index)
+        {
+            var matches = indexRegex.Match(path);
+            if (matches.Success)
+            {
+                var res = matches.Groups["index"].Value;
+                if (int.TryParse(res, out index)) return true;
+            }
+            index = -1;
+            return false;
+        }
+
+        public static bool TryGetLastIndex(string path, out int index, out string propertyName)
+        {
+            var match = indexAndListRegex.Match(path);
+            if (match.Success)
+            {
+                propertyName = match.Groups["listname"].Value;
+                var indexString = match.Groups["index"].Value;
+                if (int.TryParse(indexString, out index)) return true;
+            }
+            index = -1;
+            propertyName = null;
+            return false;
+        }
+    }
+}
