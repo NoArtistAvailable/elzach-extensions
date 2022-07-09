bc4e5651cc573e796a7e537e6bd1674050892594
zach <felix.reinemann@gmail.com>
2022-07-09 15:03:48 +0200

    [AnimatableChildren] cleanup

--- Runtime/Animatable/AnimatableChildren.cs
+++ Runtime/Animatable/AnimatableChildren.cs
@@ -1,20 +1,10 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine;
 
 namespace elZach.Common
 {
     public class AnimatableChildren : AnimatableMultiple
-    {
-#pragma warning disable CS4014
+    { 
         public override IEnumerable<Transform> Targets => transform.GetChildren();
-#pragma warning restore CS4014
-        
     }
 }
